3e1a13a4c0034d10d428a8bda65a15abddf7f70f
neica200 <marioneica200@gmail.com>
2024-11-19 22:29:20 +0200

    check hosts

--- check_hosts.sh
+++ check_hosts.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-cat /etc/hosts | while read -r line
-do
-    if [[ $line == \#* || -z $line ]]
-    then
+validate_ip_dns() {
+    local hostname=$1
+    local ip=$2
+    local dns_server=$3
+
+    local resolved_ip=$(nslookup $hostname $dns_server 2>/dev/null | grep -A 1 "Name:" | grep "Address" | tail -n 1 | awk '{print $2}')
+
+
+    if [[ -z $resolved_ip ]]; then
+        echo "Eroare: Nu s-a putut rezolva $hostname folosind $dns_server."
+        return 1
+    fi
+
+    if [[ $resolved_ip != $ip ]]; then
+        echo "Bogus IP pentru $hostname! Așteptat: $ip, Rezolvat: $resolved_ip"
+        return 1
+    fi
+
+    echo "Asocierea dintre $hostname și $ip este validă."
+    return 0
+}
+
+cat /etc/hosts | while read -r line; do
+    if [[ $line == \#* || -z $line ]]; then
         continue
     fi
 
     ip=$(echo $line | awk '{print $1}')
     hostname=$(echo $line | awk '{print $2}')
 
-    resolved_ip=$(nslookup $hostname 2>/dev/null | grep -A 1 "Name:" | grep "Address" | tail -n 1 | awk '{print $2}')
-
-    if [[ -z $resolved_ip ]]
-    then
-        echo "Eroare"
-        continue
-    fi
-
-    if [[ $resolved_ip != $ip ]]
-    then
-        echo "Bogus IP for $hostname in /etc/hosts!"
-    fi
+    validate_ip_dns $hostname $ip "8.8.8.8"
 done
